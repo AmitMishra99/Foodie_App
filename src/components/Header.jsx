e58e830588a500b5149185a85c630f69409787ac
AmitMishra99 <aamitmishra364@gmail.com>
2025-07-25 12:55:32 +0530

    Menu created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <div className="absolute flex flex-col items-start max-w-[50%] gap-[1.5vw] bottom-[10%] left-[6vw] animation">
                 <h2 className="font-medium text-white text-[4.4vw]">Order your favourite food here</h2>
                 <p className="text-white font-[1vw] ">Choose from a diverce menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertises . our delicious meal at a time </p>
-                <button className="border-none font-medium text-[1.1vw] text-[#747474]  bg-white rounded-[50px] px-4.5 py-2.5">View Menu</button>
+                <button className="border-none font-medium text-[1.1vw] text-[#747474]  bg-white rounded-[50px] px-4.5 py-2.5 cursor-pointer">View Menu</button>
         </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Header from "../components/Header"
+import Menu from "../components/Menu"
 
 const Home = () => {
   return (
     <div className="home" >
       <Header/>
+      <Menu/>
       
     </div>
   )
